26963b9115c12283cd60cbaf305480422e78c671
Kalebandri <kalebandri.contact@gmail.com>
2016-12-07 21:12:09 -0800

    Tugas
    Tugas

---
+++ src/ReversalClient.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class ReversalClient {
+	public static void main(String[] args) throws IOException {
+		try (Socket s = new Socket("192.168.56.1", 9090)) {
+			Scanner cin = new Scanner(System.in);
+			BufferedReader in = new BufferedReader(
+					new InputStreamReader(s.getInputStream())
+					);
+			PrintWriter out = new PrintWriter(
+					s.getOutputStream(),
+					true
+					);
+			
+			String response = "" , request = "";
+			for (int i = 0; i < 3; i++) {
+				response = in.readLine();
+				System.out.println(response);
+				
+			}
+			
+			while (true) {
+				System.out.print(">> ");
+				request = cin.nextLine(); // user ketik input
+				
+				out.println(request); // kirim ke server
+				
+				response = in.readLine(); // baca respon server
+				if (response == null || response.equals(""))
+					break;
+				System.out.println(response);
+				
+			}
+			System.out.println("Connection closed.");
+			
+		}
+}
+}
